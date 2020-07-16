4ec78085578bd5c35a17ab9387c7df303a46fe5f
LuEdu87 <87leel@gmail.com>
2020-07-16 19:48:34 +0200

    Goal: Eliminated useless comments in main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,13 +6,7 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
- * All rights reserved.</center></h2>
  *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
  *
  ******************************************************************************
  */
@@ -26,11 +20,7 @@
 #include "LCD_SD.h"
 #include "LCD_SCREEN.h"
 #include "LCD_TOUCHSCREEN.h"
-//extern const unsigned char Kurumi[307209];
-//extern const unsigned char Kurumi_Conv_Izq_a_Der[307209];
-//extern const unsigned char Kurumi_Toalla[307209];
-//extern const unsigned char Kurumi_Escor[307209];
-//extern const unsigned char Kurumi_cortinas[307209];
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -52,9 +42,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-//ADC_HandleTypeDef hadc1;
 
-//SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
 LCD_SCREEN_Handle_t hlcd;
@@ -78,11 +66,9 @@ char *FileSD[]={"Super_ciudad.bin",
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-//static void MX_ADC1_Init(void);
-//static void MX_SPI1_Init(void);
+
 /* USER CODE BEGIN PFP */
-//void PintarKurumi(uint8_t *Picture,uint32_t Largo);
-//void DibujarTest(uint16_t Xo,uint16_t  Yo,char Letters[],uint8_t size);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -124,8 +110,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-//  MX_ADC1_Init();
-  //MX_SPI1_Init();
+
   /* USER CODE BEGIN 2 */
   LCD_SCREEN_Init(hlcd);
 
@@ -141,7 +126,37 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  LCD_SD_Mount(&hsd);
+  	LCD_SD_Begin(&hsd);
+  	Valor=sizeof(FileSD)/sizeof(FileSD[0]);
 
+
+
+  	while(1)
+  	{
+
+  		Almacen = 0;
+  		LCD_SD_FileOpen(&hsd, FileSD[i]);
+  		while(Almacen<6)
+  		{
+  			Almacen=LCD_Touch_CoorX(&htch);
+  		}
+
+  		if(Almacen>250)
+  		{
+  			i++;
+  			if(i>=Valor)i=0;
+  		}else
+  		{
+  			i--;
+
+  			if(i<0)
+  			{
+  				i=Valor-1;
+  			}
+  		}
+
+  	}
   /* USER CODE END 2 */
 
 
@@ -149,41 +164,7 @@ int main(void)
 
 
 
-	LCD_SD_Mount(&hsd);
-	LCD_SD_Begin(&hsd);
-	Valor=sizeof(FileSD)/sizeof(FileSD[0]);
-	//Valor=sizeof(FileSD);
 
-
-	while(1)
-	{
-
-		Almacen = 0;
-		LCD_SD_FileOpen(&hsd, FileSD[i]);
-		while(Almacen<6)
-		{
-			Almacen=LCD_Touch_CoorX(&htch);
-		}
-
-		if(Almacen>250)
-		{
-			i++;
-			if(i>=Valor)i=0;
-		}else
-		{
-			i--;
-
-			if(i<0)
-			{
-				i=Valor-1;
-			}
-		}
-
-
-
-		//i++;
-		//HAL_Delay(100);
-	}
 
 
 
@@ -220,12 +201,12 @@ int main(void)
 
 
 
-	//LCD_SD_Send(&hlcd, SEND_OP_COND);
 
 
 
 
-	while (1) ;
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -281,50 +262,7 @@ void SystemClock_Config(void)
   * @param None
   * @retval None
   */
-//static void MX_ADC1_Init(void)
-//{
-//
-//  /* USER CODE BEGIN ADC1_Init 0 */
-//
-//  /* USER CODE END ADC1_Init 0 */
-//
-//  ADC_ChannelConfTypeDef sConfig = {0};
-//
-//  /* USER CODE BEGIN ADC1_Init 1 */
-//
-//  /* USER CODE END ADC1_Init 1 */
-//  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-//  */
-//  hadc1.Instance = ADC1;
-//  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
-//  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-//  hadc1.Init.ScanConvMode = DISABLE;
-//  hadc1.Init.ContinuousConvMode = DISABLE;
-//  hadc1.Init.DiscontinuousConvMode = DISABLE;
-//  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-//  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-//  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-//  hadc1.Init.NbrOfConversion = 1;
-//  hadc1.Init.DMAContinuousRequests = DISABLE;
-//  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-//  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-//  */
-//  sConfig.Channel = ADC_CHANNEL_4;
-//  sConfig.Rank = 1;
-//  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
-//  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  /* USER CODE BEGIN ADC1_Init 2 */
-//
-//  /* USER CODE END ADC1_Init 2 */
-//
-//}
+
 
 /**
   * @brief SPI1 Initialization Function
@@ -341,28 +279,7 @@ void SystemClock_Config(void)
 //  /* USER CODE BEGIN SPI1_Init 1 */
 //////
 //  /* USER CODE END SPI1_Init 1 */
-//  /* SPI1 parameter configuration*/
-//  hspi1.Instance = SPI1;
-//  hspi1.Init.Mode = SPI_MODE_MASTER;
-//  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-//  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-//  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-//  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-//  hspi1.Init.NSS = SPI_NSS_SOFT;
-//  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-//  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-//  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-//  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-//  hspi1.Init.CRCPolynomial = 10;
-//  if (HAL_SPI_Init(&hspi1) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  /* USER CODE BEGIN SPI1_Init 2 */
-//////
-//  /* USER CODE END SPI1_Init 2 */
-//
-//}
+
 
 /**
   * @brief GPIO Initialization Function
@@ -380,64 +297,12 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void DibujarTest(uint16_t Xo,uint16_t  Yo,char Letters[],uint8_t size)
-{
-	//uint8_t CntChrt;
-	uint8_t Letra_a[]={0x00,0x41,0x41,0x63,0x63,0x63,0x63,0x55,0x55,0x55,0x55,0x49,0x49,0x49,0x00};/*"m",0*/
-	HAL_Delay(100);
-
-	for(int i=0;i<15;i++)
-	{
-		for(int j=0;j<8;j++)
-		{
-			if((Letra_a[i]>>j)&1)
-			{
-
-				LCD_SCREEN_Coordenates(Xo+(j*size), Yo+(i*size), Xo+(j*size)+(size-1), Yo+(i*size)+(size-1));
-				LCD_SCREEN_WriteDataSingle(MemoryWriteCMD, Comando);
-				for(uint8_t k=0; k<(size*size);k++)
-				{
-					LCD_SCREEN_WriteDataSingle((uint8_t)(Violeta>>8), Data);
-					LCD_SCREEN_WriteDataSingle((uint8_t)Violeta, Data);
-				}
-			}else
-			{
-				LCD_SCREEN_Coordenates(Xo+(j*size), Yo+(i*size), Xo+(j*size)+(size-1), Yo+(i*size)+(size-1));
-				LCD_SCREEN_WriteDataSingle(MemoryWriteCMD, Comando);
-				for(uint8_t k=0; k<(size*size);k++)
-				{
-					LCD_SCREEN_WriteDataSingle((uint8_t)0xFF, Data);
-					LCD_SCREEN_WriteDataSingle((uint8_t)0xFF, Data);
-				}
-			}
-		}
-	}
-
-
-}
 
 
 
 
-//void PintarKurumi(uint8_t *Picture,uint32_t Largo)
-//{
-//	//int SizeColor;
-//
-//	//SizeColor =sizeof(*Picture)/sizeof(*Picture[0]);
-//	HAL_Delay(1000);
-//	//LCD_WriteData((int*)Picture, Largo);
-//
-//
-//	LCD_WriteDataSingle(0x2C, Comando);
-//
-//	for(int i=0;i<Largo;i++)
-//	{
-//		LCD_WriteDataSingle(Picture[i],Data);
-//
-//	}
-//	LCD_WriteDataSingle(0x00, Comando);
-//
-//}
+
+
 /* USER CODE END 4 */
 
 /**
